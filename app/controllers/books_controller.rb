1b12ac05a42fe3b44bebd55cae6d8f02de449e19
kawasakimaki <kchurip.08@gmail.com>
2023-08-10 14:07:15 +0000

    [Add]bookのcreate機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,5 +1,13 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
+  end
+
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/books/show'
+
   end
 
   def index
@@ -9,5 +17,10 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
